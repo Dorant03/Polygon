70d1d80b090b06bbb7267b5408906e7769ef499a
Mirroslava <31761632+Mirroslava@users.noreply.github.com>
2017-11-23 18:33:35 +0200

    Point.cpp

---
+++ Point.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include "Point.h"
+
+using namespace std;
+
+Point::Point()
+{
+	x=0;
+	y=0;
+}
+/*Point::Point(istream &ob)
+{
+	ob>>x;
+	ob>>y;
+}*/
+Point::Point(double _x, double _y)
+{
+	x=_x*20+500;
+	y=_y*(-1)*20+300;
+}
+double Point::getX()
+{
+	return x;
+}
+double Point::getY()
+{
+	return y;
+}
+Point:: ~Point()
+{
+	x=0;
+	y=0;
+}
