706b4db9fd0032f0ef1cffc41ff0695c7772604c
Dorant03 <wolodymyrsydorko@gmail.com>
2017-11-24 00:30:22 +0200

    change Point

---
+++ Point.cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+#include "Point.h"
+
+using namespace std;
+
+Point::Point()
+{
+	x=0;
+	y=0;
+}
+Point::Point(const Point &ob)
+{
+	this->x=ob.x;
+	this->y=ob.y;
+}
+Point::Point(double _x, double _y)
+{
+	x=_x;
+	y=_y;
+}
+double Point::getX()const
+{
+	return x;
+}
+double Point::getY()const
+{
+	return y;
+}
+void Point::setX(double _x)
+{
+	x=_x;
+}
+void Point::setY(double _y)
+{
+	y=_y;
+}
+Point Point::operator+(const Point &ob)
+{
+	Point point;
+	point.setX(x+ob.getX());
+	point.setY(y+ob.getY());
+	return point;
+}
+Point& Point::operator+=(const Point &ob)
+{
+	this->x+=ob.getX();
+	this->y+=ob.getY();
+	return *this;
+}
+Point Point::operator-(const Point &ob)
+{
+	Point point;
+	point.setX(x-ob.getX());
+	point.setY(y-ob.getY());
+	return point;
+}
+Point& Point::operator-=(const Point &ob)
+{
+	this->x-=ob.getX();
+	this->y-=ob.getY();
+	return *this;
+}
+Point Point::operator*(double k)
+{
+	Point point;
+	point.setX(x*k);
+	point.setY(y*k);
+	return point;
+}
+Point& Point::operator*=(double k)
+{
+	this->x*=k;
+	this->y*=k;
+	return *this;
+}
+Point& Point::operator=(const Point &ob)
+{
+	this->x=ob.getX();
+	this->y=ob.getY();
+	return *this;
+}
+Point:: ~Point()
+{
+	x=0;
+	y=0;
+}
---
+++ Point.h
@@ -0,0 +1,29 @@
+#pragma once
+#include <iostream>
+#ifdef Point_EXPORTS
+#define Point_API __declspec(dllexport) 
+#else
+#define Point_API __declspec(dllimport) 
+#endif
+class Point
+{
+protected:
+	double x;
+	double y;
+public:
+	Point_API Point();
+	Point_API Point(const Point &ob);
+	Point_API Point(double _x, double _y);
+	Point_API double getX()const;
+	Point_API void setX(double x);
+	Point_API void setY(double y);
+	Point_API double getY()const;
+	Point_API Point& operator=(const Point &ob);
+	Point_API Point operator+(const Point &ob);
+	Point_API Point& operator+=(const Point &ob);
+	Point_API Point operator-(const Point &ob);
+	Point_API Point& operator-=(const Point &ob);
+	Point_API Point operator*(double k);
+	Point_API Point& operator*=(double k);
+	Point_API ~Point();
+};
