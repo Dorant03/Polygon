c756b6fb4b559cd8e2485488a851280309715657
Dinara227 <32163613+Dinara227@users.noreply.github.com>
2017-11-23 22:41:32 +0200

    Update Poly.h

--- Poly.h
+++ Poly.h
@@ -1,1 +1,16 @@
-
+#pragma once
+#include "Point.h"
+#include "List.h"
+#ifdef Stack_EXPORTS
+#define Polygon_API __declspec(dllexport) 
+#else
+#define Polygon_API __declspec(dllimport) 
+#endif 
+class Poly
+{
+protected:
+	List<Point> * points;
+public:
+	Polygon_API Poly();
+	Polygon_API Poly(List<Point> * points);
+};
