480e68c1ed06ee036443697907a0503491a021a6
Dinara227 <32163613+Dinara227@users.noreply.github.com>
2017-11-23 22:43:59 +0200

    Update Poly.cpp

--- Poly.cpp
+++ Poly.cpp
@@ -1,1 +1,34 @@
+#include <iostream>
+#include "Poly.h"
+
+using namespace std;
+
+Poly::Poly()
+{
+	points=new List<Point>;
+}
+Poly::Poly(List<Point> * points)
+{
+
+}
+List<Point> * Poly::getPoints()
+{
+	return points;
+}
+void Poly::addPoint(double _x, double _y)
+{
+	points->puchBack(Point(_x, _y));
+}
+void Poly::addPoint()
+{
+	double x; double y;
+	cout<<"Enter x,y: "; cin>>x>>y;
+	points->puchBack(Point(x, y));
+}
+Poly::~Poly()
+{
+	//delete [] points;
+}
+
+
 
